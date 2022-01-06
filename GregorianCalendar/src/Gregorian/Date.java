2c57c5fd539cdb295d3d4539ed776377b11b9174
ratataca <55879896+ratataca@users.noreply.github.com>
2022-01-06 17:15:21 +0900

    Update Date.java

--- GregorianCalendar/src/Gregorian/Date.java
+++ GregorianCalendar/src/Gregorian/Date.java
@@ -5,9 +5,9 @@ public class Date {
 	private int month;
 	private int day;
 	
-	public void setYear(int year) 	{ 	this.year  = year;	}
-	public void setMonth(int month) { 	this.month = month;	}
-	public void setDay(int day) 	{ 	this.day   = day;	}
+	public void setYear(int year) 		{ 	this.year  = year;	}
+	public void setMonth(int month) 	{ 	this.month = month;	}
+	public void setDay(int day) 		{ 	this.day   = day;	}
 	
 	
 	public int getYear() 			{	return this.year;	}
